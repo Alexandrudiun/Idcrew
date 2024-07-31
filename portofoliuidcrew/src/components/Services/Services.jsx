4297442b22eef7b97b23c6cf27d2df1558f7f2e5
Alexandru Dinu <alexandrudinu04@gmail.com>
2024-07-31 13:11:06 +0200

    slidere

--- portofoliuidcrew/src/components/Services/Services.jsx
+++ portofoliuidcrew/src/components/Services/Services.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import './Services.css';
+import Slider from './Slider';
 
-function Services() {   
-    return (<>
+function Services() {
+    const [isExpanded, setIsExpanded] = useState(false);
 
-        <div className="services">
-        
-            <h1>Our Services</h1>
-            <button className="btn">View All</button>
+    const handleButtonClick = () => {
+        setIsExpanded(!isExpanded);
+    };
 
+    return (
+        <div className={`servicesexpanded ${isExpanded ? "expanded" : ""}`}>
+            <div className="services">
+                <h1>Our Services</h1>
+                <button className="btn" onClick={handleButtonClick}>
+                    {isExpanded ? "Collapse" : "View All"}
+                </button>
+            </div>
+            {isExpanded && <Slider />}
         </div>
-
-    </>);
-}   
+    );
+}
 
 export default Services;
