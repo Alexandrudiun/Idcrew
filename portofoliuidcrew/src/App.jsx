c60c00c14877d64f9666e4ff92e9fe5b40d2498b
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2024-08-01 14:03:08 +0300

    hero nav done

--- portofoliuidcrew/src/App.jsx
+++ portofoliuidcrew/src/App.jsx
@@ -1,24 +1,29 @@
-import HomePage from './components/HomePage'
-import PreLoader from './components/preloader/PreLoader'
-import '../src/style.css'
-import Navbar from './components/navbar/Navbar'
-import Services from './components/Services/Services'
-import Hero from './components/hero/Hero'
+import React from 'react';
+import HomePage from './components/HomePage';
+import PreLoader from './components/preloader/PreLoader';
+import '../src/style.css';
+import Navbar from './components/navbar/Navbar';
+import Services from './components/Services/Services';
+import Hero from './components/hero/Hero';
+import HorizontalScroll from 'react-scroll-horizontal';
 
 function App() {
+  const childStyle = { width: '100vw', height: '100%' };
 
   return (
     <div className="app">
-      {/* Content */}
-      <Navbar />
-      <Hero />
-      {/* <HomePage/> */}
-      <Services/>
-      <br></br>
-      {/* Preload */}
+      <HorizontalScroll>
+        <div style={childStyle} className="main bg">
+          <Navbar />
+          <Hero />
+        </div>
+        <div style={childStyle} className="main bg1">
+          <Services />
+        </div>
+      </HorizontalScroll>
       <PreLoader />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
