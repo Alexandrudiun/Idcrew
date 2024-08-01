62f6cc9dc198de8385de963d2e4c40379d410a8d
Alexandru Dinu <alexandrudinu04@gmail.com>
2024-08-01 16:11:15 +0200

    Particles

--- portofoliuidcrew/src/App.jsx
+++ portofoliuidcrew/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import HomePage from './components/HomePage';
 import PreLoader from './components/preloader/PreLoader';
 import '../src/style.css';
 import Navbar from './components/navbar/Navbar';
 import Services from './components/Services/Services';
 import Hero from './components/hero/Hero';
 import HorizontalScroll from 'react-scroll-horizontal';
+import Artifica from './components/Particles/Artifica';
 
 function App() {
   const childStyle = { width: '100vw', height: '100%' };
@@ -18,8 +18,13 @@ function App() {
           <Hero />
         </div>
         <div style={childStyle} className="main bg1">
-          <Services />
+          <Services /> 
+          <Artifica  />
+  
+
         </div>
+      
+
       </HorizontalScroll>
       <PreLoader />
     </div>
