385dccce19e1a0e81238cc72c0e18e0890d63825
Alexandru Dinu <alexandrudinu04@gmail.com>
2024-08-08 14:48:57 +0200

    Proiecte

---
+++ portofoliuidcrew/src/components/Proiect/Proiect.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './Proiect.css';
+
+function Proiect({Title, Description, Image , Link}) {
+    return (
+        <>
+            <div className="card">
+
+                
+                <img src={Image} style={{maxWidth:'50%'}} alt="Avatar" />
+                <div className="container">
+                    <h4><b>{Title}</b></h4>
+                    <p>{Description}</p>
+                    <a href={Link}>View Project</a>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Proiect;
--- portofoliuidcrew/src/components/Projects/Projects.jsx
+++ portofoliuidcrew/src/components/Projects/Projects.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 import './Projects.css';
-
+import Proiect from "../Proiect/Proiect";
+import '../about/about.css'; //!important schimba asta si fa o in css ul de la projects
 function Projects() {   
     return ( 
         <>
-            <h1 style={{fontSize: '334px'}}>PROIECTE</h1>
+            <h1 style={{fontSize: '334px'}} className="btn-shine">PROIECTE</h1> 
+        <div className="flex">
+            {/* <Proiect
+             Title="Restaurant Management"
+             Description="A web application for managing a restaurant."
+             Link="www.google.com"
+             Image="https://i.imgur.com/9Y9Yq6B.png"
+            /> */}
+
+            <Proiect
+                Title="Taxi Management"
+                Description="A web application for managing a taxi company."
+                Link="www.google.com"
+                Image="https://i.imgur.com/9Y9Yq6B.png"
+            />
+            
+            <Proiect
+                Title="JSF Project"
+                Description="An online job store for students."
+                Link="www.google.com"
+                Image="https://i.imgur.com/9Y9Yq6B.png"
+            />
+
+            <Proiect
+                Title="Plug Hunt"
+                Description="An EV charging station platform for renting and finding Charging stations."
+                Link="www.google.com"
+                Image="https://i.imgur.com/9Y9Yq6B.png"
+            />
+        </div>
         </>
     );
 }
