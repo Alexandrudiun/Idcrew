46c0395bd61a6757a178d520d1c09e1e03b3ae84
ionescuaandrei <129840688+ionescuaandrei@users.noreply.github.com>
2024-08-11 13:11:19 +0300

    .

--- portofoliuidcrew/src/components/Services/Services.jsx
+++ portofoliuidcrew/src/components/Services/Services.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPencilRuler, faPhotoVideo, faFileCode, faAlignLeft, faTimes, faCheckCircle, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons';
+import { faPencilRuler, faFileCode, faAlignLeft, faTimes, faCheckCircle, faLongArrowAltRight, faMobile, faPenFancy } from '@fortawesome/free-solid-svg-icons';
 import './Services.css';
 import Slider from './Slider';
 import Restaurant from "./Restaurant";
@@ -25,43 +25,48 @@ function Services() {
                     <li className="service-container">
                         <div className="service-card" onClick={() => openModal(1)}>
                             <FontAwesomeIcon icon={faPencilRuler} className="service-icon" />
-                            <h3>UI/UX Consultancy</h3>
+                            <h3>Video Editing</h3>
                             <div className="learn-more-btn">Learn More <FontAwesomeIcon icon={faLongArrowAltRight} /></div>
                         </div>
                         <div className={`service-modal flex-center ${activeModal === 1 ? 'active' : ''}`}>
                             <div className="service-modal-body">
                                 <button className="modal-close-btn" onClick={closeModal}><FontAwesomeIcon icon={faTimes} /> Close</button>
-                                <h3>UI/UX Consultancy</h3>
-                                <h4>What is UX consulting?</h4>
-                                <p>UX consulting helps companies improve their product’s overall usability and optimize expenses by implementing the right UX processes, methods, and tools.</p>
-                                <h4>What I provide</h4>
+                                <h3>Video Editing</h3>
+                                <h4>What is Video Editing?</h4>
+                                <p>Our professional video editing team is dedicated to transforming your raw footage into polished, compelling content that stands out. Whether you're launching a new app, promoting a product, or showcasing your services, we ensure your message is delivered with impact and style</p>
+                                <h4>What We Provide</h4>
                                 <ul>
-                                    <li><FontAwesomeIcon icon={faCheckCircle} /> Establish the right UX processes</li>
-                                    <li><FontAwesomeIcon icon={faCheckCircle} /> Create exceptional user experiences</li>
-                                    <li><FontAwesomeIcon icon={faCheckCircle} /> Discover new business opportunities</li>
-                                    <li><FontAwesomeIcon icon={faCheckCircle} /> Save resources</li>
+                                    <li><FontAwesomeIcon icon={faCheckCircle} /> Seamlessly stitching your footage together to create a smooth and engaging narrative.
+                                    </li>
+                                    <li><FontAwesomeIcon icon={faCheckCircle} /> Utilizing After Effects to add dynamic visual elements that enhance your video's appeal.</li>
+                                    <li><FontAwesomeIcon icon={faCheckCircle} /> Adjusting colors to set the right tone and mood for your video, ensuring it resonates with your audience.</li>
+                                    <li><FontAwesomeIcon icon={faCheckCircle} /> Enhancing audio quality and adding sound effects that complement your visuals.</li>
+                                    <li><FontAwesomeIcon icon={faCheckCircle} /> Creating custom animations and graphics to highlight key information and make your video more informative and visually appealing.</li>
                                 </ul>
                             </div>
                         </div>
                     </li>
                     <li className="service-container">
                         <div className="service-card" onClick={() => openModal(2)}>
-                            <FontAwesomeIcon icon={faPhotoVideo} className="service-icon" />
-                            <h3>Branding & Design</h3>
+                            <FontAwesomeIcon icon={faMobile} className="service-icon" />
+                            <h3>Mobile Apps</h3>
                             <div className="learn-more-btn">Learn More <FontAwesomeIcon icon={faLongArrowAltRight} /></div>
                         </div>
                         <div className={`service-modal flex-center ${activeModal === 2 ? 'active' : ''}`}>
                             <div className="service-modal-body">
                                 <button className="modal-close-btn" onClick={closeModal}><FontAwesomeIcon icon={faTimes} /> Close</button>
-                                <h3>Branding & Design</h3>
+                                <h3>Mobile Apps</h3>
                                 <h4>What is Brand & Design?</h4>
-                                <p>Brand Design can be defined as one of the crucial marketing practices of creating the name, logo, design, and the symbolic elements related to the brand to create a distinctive identity in comparison to the other brands in the market and also providing impetus to the product differentiation.</p>
-                                <h4>What I provide</h4>
+                                <p>Our experienced development team is dedicated to delivering seamless, user-friendly, and visually appealing apps that meet your specific needs. Whether you're looking to launch a new product, enhance your services, or engage your audience with a mobile solution, we ensure your app stands out in a competitive market.</p>
+                                <h4>What We provide</h4>
                                 <ul>
-                                    <li><FontAwesomeIcon icon={faCheckCircle} /> Logo Design</li>
-                                    <li><FontAwesomeIcon icon={faCheckCircle} /> Banner Design</li>
-                                    <li><FontAwesomeIcon icon={faCheckCircle} /> Visual Identity Packages</li>
-                                    <li><FontAwesomeIcon icon={faCheckCircle} /> Business Cards & Business Systems</li>
+                                    <li><FontAwesomeIcon icon={faCheckCircle} /> Using React Native, we build apps that run smoothly on both iOS and Android, ensuring a consistent experience for all users.</li>
+                                    <li><FontAwesomeIcon icon={faCheckCircle} /> Crafting intuitive and attractive interfaces that provide a great user experience and keep users engaged.</li>
+                                    <li><FontAwesomeIcon icon={faCheckCircle} /> Ensuring your app runs efficiently and responds quickly, providing a smooth experience even under heavy usage.</li>
+                                    <li><FontAwesomeIcon icon={faCheckCircle} /> Seamlessly connecting your app to necessary services and databases for real-time data and functionality.</li>
+                                    <li><FontAwesomeIcon icon={faCheckCircle} /> Offering continuous updates and improvements to keep your app running at its best and aligned with the latest platform updates.
+                                    </li>
+                                    
                                 </ul>
                             </div>
                         </div>
@@ -97,8 +102,8 @@ function Services() {
                     </li>
                     <li className="service-container">
                         <div className="service-card" onClick={() => openModal(4)}>
-                            <FontAwesomeIcon icon={faAlignLeft} className="service-icon" />
-                            <h3>Content Writing</h3>
+                            <FontAwesomeIcon icon={faPenFancy} className="service-icon" />
+                            <h3>Logo & Slogans</h3>
                             <div className="learn-more-btn">Learn More <FontAwesomeIcon icon={faLongArrowAltRight} /></div>
                         </div>
                         <div className={`service-modal flex-center ${activeModal === 4 ? 'active' : ''}`}>
